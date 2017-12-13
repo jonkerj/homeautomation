0ab0776ab62afb24b70faa31f83a09f0ee6b874c
Jorik Jonker <jorik@kippendief.biz>
2017-12-13 13:58:35 +0100

    Introduce new boot script

--- roles/sunxi/templates/boot.cmd
+++ roles/sunxi/templates/boot.cmd
@@ -1,23 +1,31 @@
-echo Loading zImage
-ext4load mmc {{ sunxi_mmc }} 0x46000000 boot/zImage${devsuffix}
-echo Loading FDT blob
-ext4load mmc {{ sunxi_mmc }} 0x49000000 boot/opi.dtb${devsuffix}
-if test -n ${devsuffix}
-then
-	echo Unsetting devsuffix, reboot proofing
-	setenv devsuffx
-	saveenv
-fi
+setenv kernel_addr 0x46000000
+setenv fdt_addr 0x49000000
+setenv scratch_addr 0x43200000
+setenv vars boot.txt
 
-setenv patchaddr 0x43200000
-setenv patchscript /boot/patch.scr
-if test -e mmc {{ sunxi_mmc }} ${patchscript}
-then
-	echo Found patch script, running it
-	load mmc {{ sunxi_mmc }} ${patchaddr} ${patchscript}
-	source ${patchaddr}
-fi
+# this may be overridden in boot.txt
+setenv bootdevtype mmc
+setenv prefix /boot
+setenv bootdev 1
+setenv bootpart 1
 
-echo All done, booting in 3 seconds
-sleep 3
-bootz 0x46000000 - 0x49000000
+echo "Loading variables (${prefix}/${vars})"
+load ${bootdevtype} ${bootdev}:${bootpart} ${scratch_addr} ${prefix}/${vars}
+env import -t ${scratch_addr} ${filesize}
+
+echo "Loading kernel (${prefix}/${kernel})"
+ext4load ${bootdevtype} ${bootdev} ${kernel_addr} ${prefix}/${kernel}
+echo "Loading FDT (${prefix}/${dtb})"
+ext4load ${bootdevtype} ${bootdev} ${fdt_addr} ${prefix}/${dtb}
+fdt addr 0x49000000
+fdt resize 65536
+
+for overlay_name in ${overlays}; do
+	load ${bootdevtype} ${bootdev}:${bootpart} ${scratch_addr} ${prefix}/${overlay_name}.dtbo
+	echo "Applying DT overlay (${prefix}/${overlay_name}.dtbo)"
+	fdt apply ${scratch_addr}
+done
+
+echo "Booting in 5 seconds"
+sleep 5
+bootz ${kernel_addr} - ${fdt_addr}
